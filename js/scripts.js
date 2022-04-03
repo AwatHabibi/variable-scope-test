dc32077e26b4bcebdc956cef7e449f4efc57408d
Alex Habibi <AwatHabibi@icloud.com>
2022-04-03 13:23:08 -0400

    Global variables

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+
+function sampleFunction() {
+  localString = "This is a local variable";
+  alert(localString);
+  localString = "This is a local variable update!!";
+  alert(localString);
+}
+
+sampleFunction();
+alert(localString);
